7134ac7b9878cbe0e1e07b2bdf723ae1684e75a7
Falguni Das <dasshuvo7@gmail.com>
2021-05-12 09:38:17 +0600

    Add new environment.

--- tic_tac_toe/envs/__init__.py
+++ tic_tac_toe/envs/__init__.py
@@ -1,2 +1,3 @@
 from tic_tac_toe.envs.tic_tac_toe_env import TicTacToeEnv
+from tic_tac_toe.envs.tic_tac_toe_env_epsilon_stochastic import TicTacToeEnvEpsilonStochastic
 from tic_tac_toe.envs.tic_tac_toe_env_stochastic import TicTacToeEnvStochastic
